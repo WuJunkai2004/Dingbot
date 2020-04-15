660decea8453dab7928a323fde6883dc438dbb53
Wu Junkai <50454344+WuJunkai2004@users.noreply.github.com>
2020-04-15 10:00:07 +0800

    Update send.py

--- send.py
+++ send.py
@@ -15,6 +15,7 @@ with open('config.json','r') as fin:
     url,secret=eval(fin.read())
 
 def urls():
+    '获取url'
     timestamp          = long(round(time() * 1000))
     secret_enc         = bytes(secret).encode('utf-8')
     string_to_sign     = '{}\n{}'.format(timestamp, secret)
@@ -23,19 +24,19 @@ def urls():
     sign               = plus(base(hmac_code))
     return url+'&timestamp=%s&sign=%s'%(timestamp,sign)
 
-def mail(msg):
+def send(msg):
+    '发送消息'
     requ=seal(urls(),data=json(msg),headers={'Content-Type': 'application/json' })
-    print post(requ).read().decode('utf-8')
+    print eval(post(requ).read())
 
 
-def text(message,*at):
-    phone=()
+def text(message,at=[]):
+    phone=[]
     every=False
-    if(at):
-        if(at[0]==all):
-            every=True
-        else:
-            phone=list(map(str,at))
+    if(at==all):
+        every=True
+    else:
+        phone=map(str,at)
     msg={
         'msgtype':'text',
         'text'   :{
@@ -46,32 +47,28 @@ def text(message,*at):
             'isAtAll'  :every 
             }
         }
-    return mail(msg)
+    return send(msg)
 
 
-def link(title,text,url,*pic):
-    img=''
-    if(pic):
-        img=pic[0]
+def link(title,text,url,pic=''):
     msg={
         'msgtype':"link",
         'link'   :{
             'title':title,
             'text' :text,
             'messageUrl':url,
-            'picUrl'    :img
+            'picUrl'    :pic
             }
         }
-    return mail(msg)
+    return send(msg)
 
-def markdown(title,text,*at):
-    phone=()
+def markdown(title,text,at=[]):
+    phone=[]
     every=False
-    if(at):
-        if(at[0]==all):
-            every=True
-        else:
-            phone=list(map(str,at))
+    if(at==all):
+        every=True
+    else:
+        phone=map(str,at)
     msg={
         "msgtype":"markdown",
         "markdown":{
@@ -83,4 +80,36 @@ def markdown(title,text,*at):
             "isAtAll"  :every
             }
         }
-    return mail(msg)
+    return send(msg)
+
+def push(title,text,*button):
+    btns=[]
+    for i in map(dict,map(lambda x:zip(('title','actionURL'),x),button)):
+        btns.append(i)
+    if(len(btns)==1):
+        ## 整体跳转ActionCard类型
+        msg={
+            "msgtype": "actionCard",
+            'actionCard':{
+                'title':title,
+                'text' :text,
+                'btnOrientation':'0',
+                'singleTitle'   :btns[0]['title'],
+                'singleURL'     :btns[0]['actionURL'],
+                }
+            }
+    else:
+        ## 独立跳转ActionCard类型
+        msg={
+            "msgtype": "actionCard",
+            "actionCard": {
+                "title": title,
+                "text" : text,
+                "btns" : btns,
+                'btnOrientation':'0',
+                }
+            }
+    send(msg)
+
+def feed():
+    pass
