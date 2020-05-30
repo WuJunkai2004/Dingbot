a46eaf8dc2f72b3e47b53cc9b4dd810b6920900f
Wu Junkai <50454344+WuJunkai2004@users.noreply.github.com>
2020-05-30 17:56:55 +0800

    Add files via upload

--- supports/dilidili.py
+++ supports/dilidili.py
@@ -1,13 +1,16 @@
 # coding=utf-8
 
-from send import *
+from dingbot import *
 from re import compile as re
 
 headers={
          'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Mobile Safari/537.36',
          }
 
-class DingbotPlus(Dingbot):
+re_title=re(r'(?<=<title>).+?(?=</title>)')
+re_hosts=re(r'.+?[^:^/](?=/)')
+
+class DingPlus(Dingbot):
 
     def share(self,url):
         if  (url.find('www.bilibili.com/video')!=-1):
@@ -17,23 +20,24 @@ class DingbotPlus(Dingbot):
         elif(url.find('www.bilibili.com/ranking')!=-1):
             return self.bilibili_rank(url)
         else:
-            return self.text(url)
+            return self.other(url)
 
     def bilibili_video(self,url):
-        for i in re(r'(?<=<meta ).+?(?=>)').findall(get(url,headers).read()):
+        html=get(url,headers).read()
+        for i in re(r'(?<=<meta ).+?(?=>)').findall(html):
             if  (i.find('itemprop="name"')       !=-1):
                 title=re(r'(?<=content=").+(?=")').search(i).group()
             elif(i.find('itemprop="description"')!=-1):
                 text =re(r'(?<=content=").+(?=")').search(i).group()
             elif(i.find('itemprop="image"'      )!=-1):
                 image=re(r'(?<=content=").+(?=")').search(i).group()
-        return robot.link(title,text,url,image)
+        return self.link(title,text,url,image)
 
     def bilibili_read(self,url):
         html =get(url,headers).read()
-        title=re(r'(?<=<title>).+?(?=</title>)').search(html).group()
+        title=re_title.search(html).group()
         text =re(r'(?<=<meta name="description" content=").+?(?=">)').search(html).group()
-        return robot.link(title,text,url,'https://i0.hdslb.com/bfs/archive/4de86ebf90b044bf9ba2becf042a8977062b3f99.png')
+        return self.link(title,text,url,'https://i0.hdslb.com/bfs/archive/4de86ebf90b044bf9ba2becf042a8977062b3f99.png')
 
     def bilibili_rank(self,url,range=[0,10]):
         html  =get(url,headers).read()
@@ -44,12 +48,21 @@ class DingbotPlus(Dingbot):
         for i in lists[range[0]:range[1]]:
             images.append(i['pic'].replace(r'\u002F',r'/'))
             titles.append((i['title']))
-        return robot.feed(zip(titles,urls,images))
+        return self.feed(zip(titles,urls,images))
 
-    def picture(self,path=r'D:\Pictures\wall.jpg'):
+    def picture(self,path=r'D:\用户目录\Pictures\wall.jpg'):
         image=open(path,'rb')
         post()
         pass
+
+    def other(self,url):
+        try:
+            html=get(url,headers).read()
+        except:
+            return self.text(url)
+        title=re_title.search(html).group()
+        img=re_hosts.search(html).group()+r'/favicon.ico'
+        self.link(title,url,img)
 
 def save(url):
     with open('web.txt','w') as fout:
