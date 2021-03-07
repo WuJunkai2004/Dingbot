5c5abd0ef6830639ad99243fee5904171c31f8e3
Wu Junkai <wujunkai20041123@outlook.com>
2021-03-07 10:39:53 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+import dingbot
+
+try:
+    readme = open('README.md').read()
+except:
+    readme = open('README.md',encoding='utf-8').read()
+
+kw = {
+    "name": 'Dingbot',
+    "version": dingbot.__version__,
+    "description": 'Dingtalk group\'s robot API Python SDK',
+    "long_description": readme,
+    "author": 'WuJunkai',
+    "author_email": 'wujunkai20041123@outlook.com',
+    "url": 'https://github.com/WuJunkai2004/Dingbot',
+    "download_url": 'https://github.com/WuJunkai2004/Dingbo',
+    "py_modules": ['dingbot'],
+    "classifiers": [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+}
+
+setup(**kw)
