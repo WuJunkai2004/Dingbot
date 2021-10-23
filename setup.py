39f22307fe949e8681d20e53c26cfc3ba5bd44e7
Wu Junkai <wu.junkai@qq.com>
2021-10-23 18:09:06 +0800

    优化包
    对包的上传和下载进行了一些优化

--- setup.py
+++ setup.py
@@ -7,26 +7,51 @@ try:
 except:
     readme = open('README',encoding='utf-8').read()
 
-kw = {
-    "name": 'DingRobotPy',
-    "version": dingbot.__version__,
-    "description": 'Dingtalk group\'s robot API Python SDK',
-    "long_description": readme,
-    "author": 'WuJunkai',
-    "author_email": 'wujunkai20041123@outlook.com',
-    "url": 'https://github.com/WuJunkai2004/Dingbot',
-    "download_url": 'https://github.com/WuJunkai2004/Dingbot',
-    'packages':['dingbot'],
-    "classifiers": [
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
-}
+try:
+    kw = {
+        "name": 'DingRobotPy',
+        "version": dingbot.__version__,
+        "description": 'Dingtalk group\'s robot API Python SDK',
+        "long_description": readme,
+        "author": 'WuJunkai',
+        "author_email": 'wujunkai20041123@outlook.com',
+        "url": 'https://github.com/WuJunkai2004/Dingbot',
+        "download_url": 'https://github.com/WuJunkai2004/Dingbot',
+        'packages':['dingbot'],
+        "classifiers": [
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    }
 
-setup(**kw)
+    setup(**kw)
+except:
+    kw = {
+        "name": 'Dingbot',
+        "version": dingbot.__version__,
+        "description": 'Dingtalk group\'s robot API Python SDK',
+        "long_description": readme,
+        "author": 'WuJunkai',
+        "author_email": 'wujunkai20041123@outlook.com',
+        "url": 'https://github.com/WuJunkai2004/Dingbot',
+        "download_url": 'https://github.com/WuJunkai2004/Dingbot',
+        'packages':['dingbot'],
+        "classifiers": [
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    }
+
+    setup(**kw)
