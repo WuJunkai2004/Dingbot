2286172437911813fc8daa4785336fbcbfe4e474
Wu Junkai <wujunkai20041123@outlook.com>
2021-10-30 14:31:20 +0800

    Update CLI.py

--- dingbot/CLI.py
+++ dingbot/CLI.py
@@ -14,11 +14,11 @@ dingbot -setup name webhook [secret]
 dingbot -robot login/logout name1 [name2...]
 dingbot -list [all/using]
   钉钉机器人的管理模块
-  -init   -i    初始化机器人的所有参数，清除载入的机器人
+  -init   -i    初始化命令行的参数，清除载入的机器人
   -setup  -s    安装一个钉钉机器人，用于后续操作
   -robot  -r    载入或者移除机器人，可以同时指定多个。
                 当使用发送模块的时候，所有被载入的机器人都会同步发送消息。
-  -list   -l    列出所有安装或者载入的机器人
+  -list   -l    列出所有安装或者载入的机器人，默认为all
 
 dingbot -text content
 dingbot -image file-path
@@ -30,15 +30,6 @@ dingbot -link url
 
 更多帮助,请访问 https://github.com/WuJunkai2004/Dingbot'''
 
-attr=sys.argv[1:] #外部命令
-argv={}           #内部命令
-recode={          #状态码
-    'errcode':405,
-    'errmsg':'wrong'
-    }
-                  #读取配置
-config = dingbot._configure_manage()
-
 class config:
     def __init__(self, path=r'.\dingbot_cli.ini'):
         self.path = path
@@ -48,17 +39,24 @@ class config:
     
     def __getattr__(self, name):
         return self.data[name]
+    
+    def for_each():
+        for robot in self.data[u'robot']:
+            yield dingbot.DingAPI( dingbot.DingManage(robot) )
+
+
+bot_config = dingbot._config_manage()
+cli_config = config()
 
 class cmd:
     def __init__(self, opt):
         '初始化'
-        self.argv = {}
-        self.attr = opt
-        
-        self.robots = []
+        self.argv = {}      # 内部码
+        self.attr = opt     # 外部码
         
         self.option()
-        self.load()
+        
+        self.action()
     
     def option(self):
         '遍历命令，并转化'
@@ -83,19 +81,37 @@ class cmd:
         for name in self.argv.keys():
             if(name in maps.keys()):
                 self.argv[ maps[name] ] = self.argv[name]
-                
-    def load(self):
-        con = config(r'.\dingbot_cli.ini')
-        self.robots = con.robot
 
-    def form(self):
-        if({'text','image','link'}&set(self.argv.keys())):
-            return self._p
-        else:
-            return self._m
+    def action(self):
+        handle(self.argv)
+       
+class handle:
+    def __init__(self, argv):
+        for act in argv.keys():
+            if('_{}'.format(act) in self.__all__):
+                method = getattr(self,'_{}'.format(act))
+                method(argv[act])
+                break
+                
+    def _help(self,para):
+        print(help)
     
-    def _p(self):
-        if  ('text'  in self.argv.keys()):
-            post_module(self.robots,'text',content=argv['text'][0])
-        elif:
+    def _init(self,para):
+        raise None
+        
+    def _setup(self,para):
+        para += [None]
+        robot = dingbot.Manage(para[0])
+        robot.login(para[1],para[2])
+        robot.remember()
+        
+    def _robot(self,para):
+        if  (para[0].lower() == 'login' ):
+            raise None
+        elif(para[0].lower() == 'logout' ):
+            raise None
+        else:
             raise RuntimeError
+    
+    def _list(self,para):
+        para += ['all']
