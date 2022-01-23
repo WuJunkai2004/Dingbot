96532822a23a91abeb6e8fb2338b87cd172233c9
Wu Junkai <wu.junkai@qq.com>
2022-01-23 21:46:18 +0800

    新增功能

--- dingbot/Card.py
+++ dingbot/Card.py
@@ -3,68 +3,111 @@
 __version__ = '0.01.0'
 __all__ = ['BaseCard', 'FeedCard', 'ImageCard', 'ItemCard', 'LinkCard']
 
-import dingbot
+try:
+    from dingbot   import _internet_connect
+except ImportError:
+    from __init__  import _internet_connect
 
 import json
 import re
 import sys
-
-urlopen = dingbot._u.urlopen
-request = dingbot._u.Request
+from tkinter import N
+from turtle import title
 
 user = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Mobile Safari/537.36'
 head = {'User-Agent': user}
 
-from __init__ import _http_manage
-from __init__ import _http_post
-from __init__ import _http_get
-
 def _search(reg, string):
+    'using reg to search from string'
     try:
         return re.search(reg, string).group()
-    except BaseException as e:
-        raise RuntimeWarm
+    except BaseException:
+        raise RuntimeWarning
         return ''
-    
-class attr:
-    def __init__(self, name, must):
-        self.name = name
-        self.must = must
 
-    def __str__(self):
-        return self.name
-    
-    def has(self, card):
-        return self.must == false or hasattr(card, self.name)
+def _post(url, data, headers):
+    'post data to the url then get the response'
+    text = _internet_connect(url, data, headers).read()
+    text = text.decode('utf-8') if(sys.version_info.major == 3)else text
+    return text
 
-class BaseCard:
-    __all__ = []
-    __type__ = 'text'
+def _get(url, headers):
+    'get the response from url'
+    text = _internet_connect(url, None, headers).read()
+    text = text.decode('utf-8') if(sys.version_info.major == 3)else text
+    return text
+
+class _Card:
+    __dict__ = {}
+    def __getattr__(self, __name):
+        try:
+            return self.__dict__[__name]
+        except KeyError:
+            return ''
+        
+    def __setattr__(self, __name, __value):
+        self.__dict__[__name] = __value
+
+class BaseCard(_Card):
+    'Base Card for dingbot'
+    __all__  = []
+    __type__ = ''
+    __variable__ = {
+        'required' : [],
+        'optional' : []
+    }
+    def __init__(self):
+        'Initialize basic data'
+        for item in self.__all__:
+            self.__dict__[item] = None
+        self.__doc__ = ', '.join(self.__all__)
+        
+    def __call__(self, *args, **kwds):
+        'Initialize and assign'
+        for index in range(len(args)):
+            self.__dict__[ self.__all__[index] ] = args[index]
+        for index in kwds.keys():
+            self.__dict__[index] = kwds[index]
+    
+    def _is_fill(self, __list):
+        for item in __list:
+            if(not self.__dict__[item]):
+                return False
+        return True
+    
+    def is_fill_necessary(self):
+        return self._is_fill(self.__variable__['required'])
+
+    def is_fill_all(self):
+        return self._is_fill(self.__all__)
+    
+    def auto_fill(self):
+        'auto fill if something did not be filled'
+        raise RuntimeWarning('Need over')
+
     def data(self):
-        if(not self.if_full()):
-            self.auto()
-            return self.data()
+        'pack the data'
+        if(not self.is_fill_all()):
+            self.auto_fill()
         data = [ (item, getattr(self,item) ) for item in self.__all__ ]
         data = dict(data)
         return data
 
-    def is_full(self):
-        have = [ item.has(self) for item in self.__all__ ]
-        return False not in have
-    
-    def auto(self):
-        raise RuntimeError
-        
     def sent(self, api):
+        'sent the data'
         method = getattr(api, self.__type__)
         return method(**self.data())
 
 class LinkCard(BaseCard):
-    __all__ = [attr('text', True), attr('title', True), attr('messageURL', True), attr('picURL', False)]
-    def auto(self):
-        if(not hasattr(self,'messageURL')):
+    __all__  = ['text', 'title', 'messageURL', 'picURL']
+    __type__ = 'link'
+    __variable__ = {
+        'required' : ['messageURL']
+    }
+    def auto_fill(self):
+        if(not self.is_fill_necessary()):
             raise AttributeError
-        html = _http_get(self.messageURL , head)
+        html = _get(self.messageURL , head)
         ## title
         title = _search(r'title.+?title',html)
         title = _search(r'(?<=>).+?(?=<)',title)
@@ -77,10 +120,24 @@ class LinkCard(BaseCard):
         self.text = self.messageURL
 
 class ImageCard(BaseCard):
+    __all__  = ['title', 'picPATH', 'picURL']
+    __type__ = 'markdown'
+    __variable__ = {
+        'required' : ['picURL']
+    }
+    def auto_fill(self):
+        pass
+
+    def data(self):
+        if(not self.is_fill_all()):
+            self.auto_fill()
+        return {
+            'title':title,
+            'text' :'![]({})'.format(self.picURL)
+        }
+
+class ItemCard(LinkCard):
     pass
 
-class ItemCard(baseCard):
-    pass
-
-class ReedCard(BaseCard):
+class FeedCard(BaseCard):
     pass
