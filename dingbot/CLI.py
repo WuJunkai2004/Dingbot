350bf25339699d27473c15358f6d8f20a73dc68e
Wu Junkai <wu.junkai@qq.com>
2022-05-17 10:35:44 +0800

    更新命令行（未完成）
    0.12更新
    -更详细的命令帮助
    -优化植入逻辑
    -优化发送逻辑
    -优化程序判定
    -修复目前已发现的漏洞

--- dingbot/CLI.py
+++ dingbot/CLI.py
@@ -3,36 +3,41 @@
 import dingbot
 import sys
 
+ver = '0.12'
 help=u'''钉钉机器人的命令行支持。
 
-dingbot -init
-dingbot -setup name webhook [secret]
-dingbot -robot login/logout name1 [name2...]
-dingbot -list [all/using]
-  钉钉机器人的管理模块
-  -init   -i    初始化命令行的参数，清除载入的机器人
-  -setup  -s    安装一个钉钉机器人，用于后续操作
-  -robot  -r    载入或者移除机器人，可以同时指定多个。
-                当使用发送模块的时候，所有被载入的机器人都会同步发送消息。
-  -list   -l    列出所有安装或者载入的机器人，默认为all
+dingbot [options...] <content>
 
-dingbot -text content
-dingbot -image file-path
-dingbot -link url
-  钉钉机器人的发送模块
-  -text   -t    发送字符串
-  -image        发送图片
-  -link         发送链接
+options:
+  -h  -help     获取帮助
+  -i  -init     初始化命令行
+  -s  -setup    安装一个钉钉机器人
+  -r  -robot    操作已安装的钉钉机器人
+  -l  -list     列出指定的机器人
+  -t  -text     发送字符串
+      -image    发送图片
+      -link     发送链接
 
+请使用 dingbot -help <option> 获取详细解释
 更多帮助,请访问 https://github.com/WuJunkai2004/Dingbot'''
+
+deta = {
+'help':'Usage : dingbot -help <option/all>\n  option  可用的标签\n  all     所有帮助',
+'init':"初始化机器人队列, 以便重新开始",
+'setup':'',
+'robot':'',
+'list':'',
+'text':'',
+'image':'',
+'link':''}
 
 class config(dingbot._configure_manage):
     __path__ = r'.\dingbot_cli.ini' 
     __inst__ = {u'robot':[]}
 
-    def for_each(self):
+    def __call__(self):
         for robot in self.data[u'robot']:
-            yield dingbot.DingAPI( dingbot.DingManage(robot) )
+            yield dingbot.DingAPI( dingbot.Manage(robot) )
 
 
 bot_config = dingbot.config()
@@ -49,7 +54,7 @@ class cmd:
         handle(self.argv)
     
     def option(self):
-        '遍历命令，并转化'
+        '遍历命令, 并转化'
         for item in self.attr:
             if(item[0] in r'\/-'):
                 sets = item[1:]
@@ -68,7 +73,8 @@ class cmd:
             'i' : 'init',
             's' : 'setup',
             'r' : 'robot',
-            'l' : 'list'
+            'l' : 'list',
+            'v' : 'version'
         }
         for name in keys:
             if(name in maps.keys()):
@@ -76,7 +82,7 @@ class cmd:
         
        
 class handle:
-    __all__ = ['_help','_init','_setup','_robot','_list' ]
+    __all__ = ['_help','_init','_setup','_robot','_list','_version','_text' ]
     def __init__(self, argv):
         for act in argv.keys():
             if('_{}'.format(act) in self.__all__):
@@ -85,18 +91,26 @@ class handle:
                 break
                 
     def _help(self,para):
-        print(help)
+        if  (len(para)==0):
+            print(help)
+        elif(len(para)==1 and para[0].lower() in deta.keys()):
+                print(deta[para[0]])
+        elif(len(para) == 1 and para[0].lower() == 'all'):
+            for item in deta:
+                print('{} :\n{}\n------'.format(item,deta[item]))
+        else:
+            print('参数错误')
     
     def _init(self,para):
         '初始化'
-        cli_config.data = {u'robot':None}
+        cli_config.data = {u'robot':[]}
         cli_config.save()
         
     def _setup(self,para):
         '登记新的机器人'
         para += [None]
         robot = dingbot.Manage(para[0])
-        robot.login(para[1],para[2])
+        robot.login('{}={}'.format(para[1],para[2]),para[3])
         robot.remember()
         
     def _robot(self,para):
@@ -106,18 +120,31 @@ class handle:
         elif(para[0].lower() == 'logout' ):
             for item in para[1:]:
                 cli_config.data[u'robot'].remove(item)
+        elif(para[0].lower() == 'delete' or para[0].lower() == 'del'):
+            for item in para[1:]:
+                bot_config.data.pop(item)
         else:
             raise RuntimeError
+        bot_config.save()
+        cli_config.save()
     
     def _list(self,para):
-        para += ['all']
+        para += ['using']
         if( para[0] == 'all' ):
             print('\n'.join(bot_config.data.keys()))
         if( para[0] == 'using' ):
             print('\n'.join(cli_config.data[u'robot']))
 
+    def _version(self,para):
+        print('version : ' + ver)
+
+    def _text(self,para):
+        for robot in cli_config():
+            robot.text(content = para[0])
+
 
 def install():
+    '植入 dingbot'
     import re
     import os
     comm = os.getcwd() + '\\' + 'CLI.py'
@@ -136,14 +163,18 @@ def install():
             fin = open(file,'w+')
             fin.write(batc)
             fin.close()
-        os.system('cmd')
+        return os
     except:
         print('Please open with administrator privileges')
+        return None
         
 
 
-if(not sys.argv[1:]):
-    install()
+if(__name__ == '__main__'):
+    if(not sys.argv[1:]):
+        success = install()
+        if(success):
+            success.system('cmd')
 
-else:
-    c = cmd(sys.argv[1:])
+    else:
+        c = cmd(sys.argv[1:])
